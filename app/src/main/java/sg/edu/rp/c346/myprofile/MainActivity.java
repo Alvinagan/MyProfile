da40502f44214024769501e2ac3c9bc4a5f7973f
GAN JIA MIN ALVINA <17010247@rp.edu.sg>
2018-07-11 12:10:27 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/myprofile/MainActivity.java
@@ -0,0 +1,58 @@
+package sg.edu.rp.c346.myprofile;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etName;
+    EditText etGPA;
+    RadioGroup rgGender;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.editTextGPA);
+        etGPA = findViewById(R.id.editTextGPA);
+        rgGender = findViewById(R.id.radioGroupGender);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        String strName = etName.getText().toString();
+        float gpa = Float.parseFloat(etGPA.getText().toString());
+        int gender = rgGender.getCheckedRadioButtonId();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        SharedPreferences.Editor prefEdit = prefs.edit();
+
+        prefEdit.putString("myName", strName);
+        prefEdit.putFloat("myGPA", gpa);
+        prefEdit.putInt("myGender", gender);
+
+        prefEdit.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String msg = prefs.getString("myName", "No Name!");
+        float theGPA = prefs.getFloat("myGPA", 0.0f);
+        int theGender = prefs.getInt("myGender", 0);
+
+        etName.setText(msg);
+        etGPA.setText(String.valueOf(theGPA));
+        rgGender.check(theGender);
+    }
+}
